3f2f98ce88ee6cac62e6babeb40eaff4d8eba997
Maksim <maxontitan@gmail.com>
2026-08-27 17:23:33 +0300

    Complete 8 task

--- Solution.cs
+++ Solution.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Recursion
 {
@@ -48,37 +49,75 @@ namespace Recursion
         }
 
         // 5 Задание.
-        void PrintEvenValues(List<int> lst, int i = 0)
+        void PrintEvenValues(List<int> lst)
+        {
+            int i = 0;
+            DoPrintEvenValues(lst, i);
+        }
+
+        void DoPrintEvenValues(List<int> lst, int i)
         {
             if (i == lst.Count) return;
             if (lst[i] % 2 == 0) System.Console.WriteLine(lst[i]);
-            PrintEvenValues(lst, i + 1);
+            DoPrintEvenValues(lst, i + 1);
         }
 
         // 6 Задание.
-        void PrintEvenIdx(List<int> lst, int i = 0)
+        void PrintEvenIdx(List<int> lst)
+        {
+            int i = 0;
+            DoPrintEvenIdx(lst, i);
+        }
+
+        void DoPrintEvenIdx(List<int> lst, int i)
         {
             if (i >= lst.Count) return;
             System.Console.WriteLine(lst[i]);
-            PrintEvenIdx(lst, i + 2);
+            DoPrintEvenIdx(lst, i + 2);
         }
 
         // 7  Задание.
-        int FindSecondMaxValue(List<int> lst)
+        T FindSecondMaxValue<T>(List<T> lst) where T : IComparable<T>
         {
-            if (lst.Count < 2) return -1;
-            int FirstMax = lst[0];
-            int SecondMax = int.MinValue;
-            return DoFindSecondMaxValue(lst, FirstMax, SecondMax);
-
+            T FirstMax, SecondMax;
+            if (lst.Count < 2) return default(T);
+            if (lst[0].CompareTo(lst[1]) >= 0)
+            {
+                FirstMax = lst[0];
+                SecondMax = lst[1];
+            }
+            else
+            {
+                FirstMax = lst[1];
+                SecondMax = lst[0];
+            }
+            int i = 2;
+            return DoFindSecondMaxValue(lst, FirstMax, SecondMax, i);
         }
 
-        int DoFindSecondMaxValue(List<int> lst, int FirstMax, int SecondMax, int i = 1)
+        T DoFindSecondMaxValue<T>(List<T> lst, T FirstMax, T SecondMax, int i) where T : IComparable<T>
         {
             if (i == lst.Count) return SecondMax;
-            if (lst[i] >= FirstMax) return DoFindSecondMaxValue(lst, lst[i], FirstMax, i + 1);
-            if (lst[i] >= SecondMax) return DoFindSecondMaxValue(lst, FirstMax, lst[i], i + 1);
+            if (lst[i].CompareTo(FirstMax) >= 0) return DoFindSecondMaxValue(lst, lst[i], FirstMax, i + 1);
+            if (lst[i].CompareTo(SecondMax) >= 0) return DoFindSecondMaxValue(lst, FirstMax, lst[i], i + 1);
             else return DoFindSecondMaxValue(lst, FirstMax, SecondMax, i + 1);
+        }
+
+        // 8 Задание.
+        List<string> FindAllFiles(string path)
+        {
+            List<string> result = new List<string>();
+            DoFindAllFiles(path, result);
+            return result;
+        }
+
+        void DoFindAllFiles(string path, List<string> result)
+        {
+            result.AddRange(Directory.GetFiles(path));
+            foreach (string Path in Directory.GetDirectories(path))
+            {
+                DoFindAllFiles(Path, result);
+            }
         }
 
     }
